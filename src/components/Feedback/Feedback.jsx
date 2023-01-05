050dce46863c1760241c755c2a6fd5709e6ec950
Kudese <d.kuprichenkov@gmail.com>
2023-01-05 05:02:36 +0200

    LeavesFeedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,5 +1,27 @@
-export default Feedback=()=>{
+import { Component } from 'react';
+import LeaveFeeedback from './Leave feedback/LeaveFeedback';
+
+export default class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  addFedback = e => {
+    const keys = e.target.name;
+    this.setState(prevState => {
+      return { [keys]: prevState[keys] + 1 };
+    });
+  };
+  render() {
     return (
-        <div>'la'</div>
-    )
+      <LeaveFeeedback
+        addFedback={this.addFedback}
+        good={this.state.good}
+        neutral={this.state.neutral}
+        bad={this.state.bad}
+      />
+      
+    );
+  }
 }
